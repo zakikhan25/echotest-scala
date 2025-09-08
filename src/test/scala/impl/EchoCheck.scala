f2e231030ce86f627ee147c83c97c9fb33a4e96b
zakikhan25 <zkhan25@luc.edu>
2025-09-08 16:59:11 -0500

    Update EchoCheck.scala

--- src/test/scala/impl/EchoCheck.scala
+++ src/test/scala/impl/EchoCheck.scala
@@ -4,25 +4,18 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.scalatestplus.scalacheck.Checkers
 
 /**
-  * Property-based testing in ScalaTest using the Checkers mixin
-  * (preferred over using ScalaCheck directly).
-  *
-  * [[http://www.scalatest.org/user_guide/writing_scalacheck_style_properties Using ScalaTest with Checkers]]
-  */
+ * Property-based testing in ScalaTest using the Checkers mixin
+ * (preferred over using ScalaCheck directly).
+ */
 class EchoCheck extends AnyFunSuite with Checkers:
 
   val simpleEcho = new SimpleEcho
-
   val doubleEcho = new DoubleEcho
 
   test("A simple echo should echo its argument"):
-    check:
-      (a: String) =>
-        simpleEcho.echo(a) == a
+    check: (a: String) => simpleEcho.echo(a) == a
 
   test("A double echo should echo twice its argument"):
-    check:
-      (a: String) =>
-        doubleEcho.echo(a) == a + " " + a
-
+    check: (a: String) => doubleEcho.echo(a) == a + " " + a
 end EchoCheck
+
