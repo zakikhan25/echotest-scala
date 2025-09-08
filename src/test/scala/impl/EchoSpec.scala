a5f00fef3dd906a966e7e07211685aa737fd0174
zakikhan25 <zkhan25@luc.edu>
2025-09-08 17:00:43 -0500

    Update EchoSpec.scala

--- src/test/scala/impl/EchoSpec.scala
+++ src/test/scala/impl/EchoSpec.scala
@@ -4,22 +4,16 @@ import org.scalacheck.Properties
 import org.scalacheck.Prop.forAll
 
 /**
-  * Property-based testing using ScalaCheck directly.
-  *
-  * [[http://www.scalacheck.org ScalaCheck documentation]]
-  */
+ * Property-based testing using ScalaCheck directly.
+ */
 object EchoSpec extends Properties("Echo"):
 
   val simpleEcho = new SimpleEcho
-
   val doubleEcho = new DoubleEcho
 
-  property("simpleEcho") = forAll:
-    (a: String) =>
-      simpleEcho.echo(a) == a
+  property("simpleEcho") = forAll: (a: String) =>
+    simpleEcho.echo(a) == a
 
-  property("doubleEcho") = forAll:
-    (a: String) =>
-      doubleEcho.echo(a) == a + " " + a
-
+  property("doubleEcho") = forAll: (a: String) =>
+    doubleEcho.echo(a) == a + " " + a
 end EchoSpec
