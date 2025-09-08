6ea40ba1267524d44744e24843cf06cce63d6ee9
zakikhan25 <zkhan25@luc.edu>
2025-09-08 16:36:41 -0500

    Update EchoJUnit.scala

--- src/test/scala/impl/EchoJUnit.scala
+++ src/test/scala/impl/EchoJUnit.scala
@@ -1,66 +1,55 @@
-package edu.luc.cs.cs371.echo
-package impl
+package edu.luc.cs.cs371.echo.impl
 
 import org.junit.*
 import Assert.*
-
 import java.io.{ByteArrayOutputStream, PrintStream}
 import scala.jdk.CollectionConverters.*
 
 /**
-  * Plain JUnit testing without ScalaTest.
-  *
-  * Alternatively, one can use
-  * [[http://www.scalatest.org/getting_started_with_junit_4_in_scala JUnit with ScalaTest]]
-  */
+ * Plain JUnit testing without ScalaTest.
+ *
+ * Alternatively, one can use
+ * [[http://www.scalatest.org/getting_started_with_junit_4_in_scala JUnit with ScalaTest]]
+ */
 class EchoJUnit:
 
-  @Test
-  def testSimpleEmpty: Unit =
+  @Test def testSimpleEmpty: Unit =
     assertEquals("", (new SimpleEcho).echo(""))
 
-  @Test
-  def testSimpleNonempty: Unit =
+  @Test def testSimpleNonempty: Unit =
     assertEquals("hello", (new SimpleEcho).echo("hello"))
 
-  @Test
-  def testDoubleEmpty: Unit =
+  @Test def testDoubleEmpty: Unit =
     assertEquals(" ", (new DoubleEcho).echo(""))
 
-  @Test
-  def testDoubleNonempty: Unit =
+  @Test def testDoubleNonempty: Unit =
     assertEquals("hello hello", (new DoubleEcho).echo("hello"))
 
-  @Test
-  def testSimpleUsingList: Unit =
+  
+  @Test def testSimpleUsingList: Unit =
     val echos = List(new SimpleEcho)
-    val result = echos(1).echo("")
+    val result = echos(0).echo("")
     assertEquals("", result)
 
-  @Test
-  def testSimpleAlsoUsingList: Unit =
+  @Test def testSimpleAlsoUsingList: Unit =
     val echos = List(new SimpleEcho)
     try
       val result = echos(1).echo("")
       fail("should have gotten an IndexOutOfBoundsException by now!")
     catch
-      case ex: IndexOutOfBoundsException => // all good
+      case _: IndexOutOfBoundsException => // all good
 
-  // this appears to work within IntelliJ but not in sbt by itself
-  @Test
-  def testMainEndToEnd: Unit =
+  @Test def testMainEndToEnd: Unit =
     val ba = new ByteArrayOutputStream
     val os = new PrintStream(ba)
     System.setOut(os)
     main.Main.main(Array.empty[String])
-    val lines =
-      import scala.language.unsafeNulls
-      ba.toString.lines.toList.asScala
+    import scala.language.unsafeNulls
+    val lines = ba.toString.lines.toList.asScala
     assertEquals("hello", lines(0))
     assertEquals("hello hello", lines(1))
 
-  @Test
-  def testInteractiveEndToEnd: Unit =
+  @Test def testInteractiveEndToEnd: Unit =
     fail("NYI")
 
 end EchoJUnit
