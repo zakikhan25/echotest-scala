7405e613f6c8daf81a729cb9c8fc67158417516c
zakikhan25 <zkhan25@luc.edu>
2025-09-08 17:00:18 -0500

    Update EchoJUnit.scala

--- src/test/scala/impl/EchoJUnit.scala
+++ src/test/scala/impl/EchoJUnit.scala
@@ -1,4 +1,5 @@
-package edu.luc.cs.cs371.echo.impl
+package edu.luc.cs.cs371.echo
+package impl
 
 import org.junit.*
 import Assert.*
@@ -7,9 +8,6 @@ import scala.jdk.CollectionConverters.*
 
 /**
  * Plain JUnit testing without ScalaTest.
- *
- * Alternatively, one can use
- * [[http://www.scalatest.org/getting_started_with_junit_4_in_scala JUnit with ScalaTest]]
  */
 class EchoJUnit:
 
@@ -25,7 +23,7 @@ class EchoJUnit:
   @Test def testDoubleNonempty: Unit =
     assertEquals("hello hello", (new DoubleEcho).echo("hello"))
 
-  
+  /
   @Test def testSimpleUsingList: Unit =
     val echos = List(new SimpleEcho)
     val result = echos(0).echo("")
@@ -37,19 +35,20 @@ class EchoJUnit:
       val result = echos(1).echo("")
       fail("should have gotten an IndexOutOfBoundsException by now!")
     catch
-      case _: IndexOutOfBoundsException => // all good
+      case ex: IndexOutOfBoundsException => // all good
 
   @Test def testMainEndToEnd: Unit =
     val ba = new ByteArrayOutputStream
     val os = new PrintStream(ba)
     System.setOut(os)
-    main.Main.main(Array.empty[String])
-    import scala.language.unsafeNulls
-    val lines = ba.toString.lines.toList.asScala
+    edu.luc.cs.cs371.echo.main.Main.main(Array.empty[String])
+    val lines =
+      import scala.language.unsafeNulls
+      ba.toString.lines.toList.asScala
     assertEquals("hello", lines(0))
     assertEquals("hello hello", lines(1))
 
   @Test def testInteractiveEndToEnd: Unit =
     fail("NYI")
-
 end EchoJUnit
+
